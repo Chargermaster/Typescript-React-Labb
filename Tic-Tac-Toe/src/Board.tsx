8ed7e81483211187a16a76e6316210ded65d8c91
Alexander Söderwall <alexander.soderwall@gmail.com>
2024-04-17 14:45:13 +0200

    Initial board concept setup

---
+++ Tic-Tac-Toe/src/Board.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Board.css";
+
+const Board = () => {
+  return (
+    <>
+      <div className="boardBody">
+        <div className="gridContainer">
+          <div className="item">Item 1</div>
+          <div className="item">Item 2</div>
+          <div className="item">Item 3</div>
+          <div className="item">Item 4</div>
+          <div className="item">Item 5</div>
+          <div className="item">Item 6</div>
+          <div className="item">Item 7</div>
+          <div className="item">Item 8</div>
+          <div className="item">Item 9</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Board;
